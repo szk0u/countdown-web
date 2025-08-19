e2147cb150f7c26f2008f0ac13fef37ea85da468
szk0u <szk0u3@gmail.com>
2025-08-20 01:57:54 +0900

    ダークモードが作動するようにする

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
